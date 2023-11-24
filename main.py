2ed2808e8a6f7c42ea67830756c467c8d9619d69
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2023-11-24 15:09:10 +0100

    chore: works on distance from intersection
    Co-Authored-By: patrycjamormus <106437709+patrycjamormus@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-from math import cos, sin, radians
-# import pandas as pd
+# general
 import geopandas as gpd
-from utils.vectors import generator_gwiazki
 
-EPSG = 'epsg:2178'  # układ 1992
+# custom
+from utils.vectors import intersekcja  # generator_gwiazki,
+
+# CONST
+EPSG = 'epsg:2178'
 RADIUS = 300
-# 5863838.87, 7527541.11
 STARTING_POINT = {"x": 7527541.11, "y": 5863838.87}
+
+# STAR DRAWING
 # generator_gwiazki(STARTING_POINT=STARTING_POINT, epsg=EPSG , star_radius=RADIUS)
 
-# intersekcja żeby zebrać dane przecięcia jednej i drugiej warstwy
-linia_brzegu_2013 = gpd.read_file('sh_in_2013.geojson')
+# DATA PREPARING
 gwiazdka = gpd.read_file('lines.geojson')
 
-# print(gwiazdka)
-# print (linia_brzegu_2013.head())
+linie_brzegu = [
+    gpd.read_file('data/sh_in_2013.geojson'),
+    # TODO dodać nowe lata
+]
 
-punkty_przeciecia = gwiazdka.unary_union.intersection(linia_brzegu_2013.to_crs(crs=EPSG).unary_union)
-punkty_przeciecia.to_file(filename='punkty_przeciecia_2013.geojson', driver="GeoJSON")
+for idx, linia_brzegu in enumerate(linie_brzegu):
+    gwiazdka[f'distance_{idx}']: list = intersekcja(
+        linia_brzegu=linia_brzegu,
+        gwiazdka=gwiazdka,
+        STARTING_POINT=STARTING_POINT,
+        EPSG=EPSG
+    )
+
+gwiazdka.to_file('nowa_gwiazdka.geojson', driver='GeoJSON')
--- utils/vectors.py
+++ utils/vectors.py
@@ -1,8 +1,13 @@
-from math import cos, sin, radians
+# general
 import geopandas as gpd
+from math import cos, sin, radians
+from shapely.geometry import Point, LineString
 
 
 def generator_gwiazki(STARTING_POINT: dict, epsg: int, star_radius: int = 100) -> None:
+    """
+    draws a star and as a result and saves it on disc as geojson file
+    """
     results: list = []
 
     def calculate_coord(st_point: dict, angle: float, radius: int) -> dict:
@@ -27,7 +32,25 @@ def generator_gwiazki(STARTING_POINT: dict, epsg: int, star_radius: int = 100) -
     coordinates: list = [item['Coordinates'] for item in results]
     gdf = gpd.GeoDataFrame(geometry=gpd.GeoSeries.from_wkt(coordinates))
     gdf = gdf.set_crs(epsg, allow_override=True)
-    print(gdf.crs)
-    print(gdf.head)
     gdf.to_file(filename='lines.geojson', driver="GeoJSON")
-    print('zrobione')
+
+
+def intersekcja(linia_brzegu: object, gwiazdka: object, STARTING_POINT: dict, EPSG: str) -> list:
+    """
+    calculate list with distances from starting point to intersecting point
+    :param linia_brzegu: geopandas geodataframe
+    :param gwiazdka: geopandas geodataframe
+    :param STARTING_POINT: dict
+    :param EPSG: str
+    :return: list
+    """
+    linia_brzegu.to_crs(crs=EPSG, inplace=True)
+    columns_data: list = []
+    for gw_row in gwiazdka.itertuples():
+        for ln_row in linia_brzegu.itertuples():
+            point_of_intersect = gw_row.geometry.intersection(ln_row.geometry)
+            dis = point_of_intersect.distance(
+                Point(STARTING_POINT['x'], STARTING_POINT['y'])
+            )
+            columns_data.append(dis)
+    return columns_data
