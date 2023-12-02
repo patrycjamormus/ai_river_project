9ce4943d164c4e0babd81b783b124bb42f207757
patrycjamormus <106437709+patrycjamormus@users.noreply.github.com>
2023-12-02 13:26:22 +0100

    chore: data update

--- main.py
+++ main.py
@@ -16,13 +16,23 @@ generator_gwiazki(start_point=STARTING_POINT, epsg=EPSG, star_radius=RADIUS)
 gwiazdka: gpd.GeoDataFrame = gpd.read_file('lines.geojson')
 
 linie_brzegu: list[dict, dict] = [
-    {'rok': 2013, 'linia_brzegu': gpd.read_file('data/sh_in_2013.geojson')},
-    # TODO dodać nowe dane wg. powyższego wzoru
+    {'rok': 2011, 'shoreline': 'inside', 'linia_brzegu': gpd.read_file('data/in_2011.geojson')},
+    {'rok': 2011, 'shoreline': 'outside', 'linia_brzegu': gpd.read_file('data/out_2011.geojson')},
+    {'rok': 2013, 'shoreline': 'inside', 'linia_brzegu': gpd.read_file('data/in_2013.geojson')},
+    {'rok': 2013, 'shoreline': 'outside', 'linia_brzegu': gpd.read_file('data/out_2013.geojson')},
+    {'rok': 2016, 'shoreline': 'inside', 'linia_brzegu': gpd.read_file('data/in_2016.geojson')},
+    {'rok': 2016, 'shoreline': 'outside', 'linia_brzegu': gpd.read_file('data/out_2016.geojson')},
+    {'rok': 2019, 'shoreline': 'inside', 'linia_brzegu': gpd.read_file('data/in_2019.geojson')},
+    {'rok': 2019, 'shoreline': 'outside', 'linia_brzegu': gpd.read_file('data/out_2019.geojson')},
+    {'rok': 2020, 'shoreline': 'inside', 'linia_brzegu': gpd.read_file('data/in_2020.geojson')},
+    {'rok': 2020, 'shoreline': 'outside', 'linia_brzegu': gpd.read_file('data/out_2020.geojson')},
+    {'rok': 2022, 'shoreline': 'inside', 'linia_brzegu': gpd.read_file('data/in_2022.geojson')},
+    {'rok': 2022, 'shoreline': 'outside', 'linia_brzegu': gpd.read_file('data/out_2022.geojson')},
 ]
 
 for item in linie_brzegu:
     # dodaje nowa kolumne do tabeli gwiazdka
-    gwiazdka[f'distance_{item["rok"]}']: list = intersekcja(
+    gwiazdka[f'distance_{item["rok"]}_{item["shoreline"]}']: list = intersekcja(
         linia_brzegu=item["linia_brzegu"],
         gwiazdka=gwiazdka,
         starting_point=STARTING_POINT,
