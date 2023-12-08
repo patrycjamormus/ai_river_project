3d0624ddd9e0209c85eab08adc38a598f5f6ea2c
patrycjamormus <106437709+patrycjamormus@users.noreply.github.com>
2023-12-08 23:23:07 +0100

    Bugfix: Updated the intersection function

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import geopandas as gpd
 from shapely import Point
 # custom
-from utils.vectors import intersekcja, generator_gwiazki
+from utils.vectors import intersekcja, generator_gwiazdki
 
 # CONST
 EPSG: str = 'EPSG:2178'  # epsg code
@@ -10,7 +10,7 @@ RADIUS: int = 300  # meters
 STARTING_POINT: Point = Point(7527541.11, 5863838.87)  # case study area
 
 # STAR DRAWING
-generator_gwiazki(start_point=STARTING_POINT, epsg=EPSG, star_radius=RADIUS)
+generator_gwiazdki(start_point=STARTING_POINT, epsg=EPSG, star_radius=RADIUS)
 
 # DATA PREPARING
 gwiazdka: gpd.GeoDataFrame = gpd.read_file('lines.geojson')
@@ -19,21 +19,21 @@ linie_brzegu: list[dict, dict] = [
     {'rok': 2011, 'shoreline': 'inside', 'linia_brzegu': gpd.read_file('data/in_2011.geojson')},
     {'rok': 2011, 'shoreline': 'outside', 'linia_brzegu': gpd.read_file('data/out_2011.geojson')},
     {'rok': 2013, 'shoreline': 'inside', 'linia_brzegu': gpd.read_file('data/in_2013.geojson')},
-    # {'rok': 2013, 'shoreline': 'outside', 'linia_brzegu': gpd.read_file('data/out_2013.geojson')},
-    # {'rok': 2016, 'shoreline': 'inside', 'linia_brzegu': gpd.read_file('data/in_2016.geojson')},
-    # {'rok': 2016, 'shoreline': 'outside', 'linia_brzegu': gpd.read_file('data/out_2016.geojson')},
-    # {'rok': 2019, 'shoreline': 'inside', 'linia_brzegu': gpd.read_file('data/in_2019.geojson')},
-    # {'rok': 2019, 'shoreline': 'outside', 'linia_brzegu': gpd.read_file('data/out_2019.geojson')},
-    # {'rok': 2020, 'shoreline': 'inside', 'linia_brzegu': gpd.read_file('data/in_2020.geojson')},
-    # {'rok': 2020, 'shoreline': 'outside', 'linia_brzegu': gpd.read_file('data/out_2020.geojson')},
-    # {'rok': 2022, 'shoreline': 'inside', 'linia_brzegu': gpd.read_file('data/in_2022.geojson')},
-    # {'rok': 2022, 'shoreline': 'outside', 'linia_brzegu': gpd.read_file('data/out_2022.geojson')},
+    {'rok': 2013, 'shoreline': 'outside', 'linia_brzegu': gpd.read_file('data/out_2013.geojson')},
+    {'rok': 2016, 'shoreline': 'inside', 'linia_brzegu': gpd.read_file('data/in_2016.geojson')},
+    {'rok': 2016, 'shoreline': 'outside', 'linia_brzegu': gpd.read_file('data/out_2016.geojson')},
+    {'rok': 2019, 'shoreline': 'inside', 'linia_brzegu': gpd.read_file('data/in_2019.geojson')},
+    {'rok': 2019, 'shoreline': 'outside', 'linia_brzegu': gpd.read_file('data/out_2019.geojson')},
+    {'rok': 2020, 'shoreline': 'inside', 'linia_brzegu': gpd.read_file('data/in_2020.geojson')},
+    {'rok': 2020, 'shoreline': 'outside', 'linia_brzegu': gpd.read_file('data/out_2020.geojson')},
+    {'rok': 2022, 'shoreline': 'inside', 'linia_brzegu': gpd.read_file('data/in_2022.geojson')},
+    {'rok': 2022, 'shoreline': 'outside', 'linia_brzegu': gpd.read_file('data/out_2022.geojson')},
 ]
 
 for item in linie_brzegu:
     # dodaje nowa kolumne do tabeli gwiazdka
     # print(item)
-    gwiazdka[f'distance_{item["rok"]}']: list = intersekcja(
+    gwiazdka[f'distance_{item["rok"]}_{item["shoreline"]}']: list = intersekcja(
         linia_brzegu=item["linia_brzegu"],
         gwiazdka=gwiazdka,
         starting_point=STARTING_POINT,
--- utils/vectors.py
+++ utils/vectors.py
@@ -11,7 +11,7 @@ def calculate_coord(starting_point: Point, angle: float, radius: int) -> Point:
     return Point(x, y)
 
 
-def generator_gwiazki(start_point: Point, epsg: str, star_radius: int = 100) -> None:
+def generator_gwiazdki(start_point: Point, epsg: str, star_radius: int = 100) -> None:
     """
     draws a star and as a result and saves it on disc as geojson file
     """
@@ -19,14 +19,14 @@ def generator_gwiazki(start_point: Point, epsg: str, star_radius: int = 100) -> 
         LineString([
             start_point,
             calculate_coord(starting_point=start_point, angle=no, radius=star_radius)
-        ]) for no in np.arange(start=0, stop=360, step=0.1)]
+        ]) for no in np.arange(start=0, stop=360, step=1)]
 
     gdf = gpd.GeoDataFrame(geometry=gpd.GeoSeries(coordinates))
     gdf = gdf.set_crs(epsg, allow_override=True)
     gdf.to_file(filename='lines.geojson', driver="GeoJSON")
 
 
-def intersekcja(linia_brzegu: gpd.GeoDataFrame, gwiazdka: gpd.GeoDataFrame, starting_point: Point, epsg: str) -> list:
+def intersekcja(linia_brzegu: gpd.GeoDataFrame, gwiazdka: gpd.GeoDataFrame, starting_point: Point, epsg: str)-> list :
     """
     calculate list with distances from starting point to intersecting point
     :param linia_brzegu: geopandas geodataframe  - jest okodowana transformacja jednak lepiej żeby dane były w 2178
@@ -39,9 +39,9 @@ def intersekcja(linia_brzegu: gpd.GeoDataFrame, gwiazdka: gpd.GeoDataFrame, star
     columns_data: list = []
     line_geometry = linia_brzegu.iloc[0].geometry #wyciaganie wspolrzednych
 
-    for _, gw_row in gwiazdka.itertuples():
-        print()
-        # point_of_intersect = gw_row.intersection(line_geometry)
-        # dist = point_of_intersect.distance(starting_point)
-        # columns_data.append(dist)
+
+    for gw_row in gwiazdka.itertuples():
+        point_of_intersect = gw_row.geometry.intersection(line_geometry)
+        dist = point_of_intersect.distance(starting_point)
+        columns_data.append(dist)
     return columns_data
