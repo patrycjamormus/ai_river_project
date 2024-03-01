2be03cdaa98e2b877a778454ee382ec4075cfaa6
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2024-03-01 13:16:40 +0100

    wip: new processing approach

--- utils/nw_input_data_prep.py
+++ utils/nw_input_data_prep.py
@@ -3,7 +3,7 @@ from shapely.geometry import Point
 
 def prep_ai_leaning_dataset(input:gpd.GeoDataFrame)->gpd.GeoDataFrame:
     point_list: gpd.GeoDataFrame = input.explode(ignore_index=True)
-    point_list.to_file('../.outputs/data_model_xy.geojson', driver='GEOJSON')
+    point_list.to_file('./.outputs/data_model_xy.geojson', driver='GeoJSON')
     return point_list.get_coordinates().reset_index()
 
 
