b4c21b391cab189523ff94d4cb19b8ae9ddd817c
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2024-03-01 11:36:20 +0100

    wip: new data model

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import geopandas as gpd
 
+
+from utils.nw_input_data_prep import prep_ai_leaning_dataset
+
+
+INPUT = gpd.read_file('.inputs/in_out_geojson/in_2022.geojson')
 def main():
     # 1. przygotowuje dane do uczenia - dane muszą obsługiwać współrzędne x,y
+    prep_ai_leaning_dataset(input=INPUT)
 
 
 
